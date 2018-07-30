6be5c6a99d5ff90442a05775fb3a2f4d7e5720b8
zhangzhichao <zhangzhichaolove@vip.qq.com>
2018-07-30 17:18:38 +0800

    update FileUriExposedException error

--- app/src/main/java/rxjava/example/App.java
+++ app/src/main/java/rxjava/example/App.java
@@ -2,6 +2,8 @@ package rxjava.example;
 
 import android.app.Activity;
 import android.app.Application;
+import android.os.Build;
+import android.os.StrictMode;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -22,6 +24,11 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
+        //初始化置入一个不设防的VmPolicy
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {//TODO Android N 会抛出FileUriExposure  URI暴露异常
+            StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+            StrictMode.setVmPolicy(builder.build());
+        }
     }
 
 
